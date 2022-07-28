9d0a25195609b71f411ca2165191f508a686cfd3
Shakil Ahmmed <shakilfci461@gmail.com>
2022-07-29 03:50:51 +0600

    Login Refactor

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\Admin\Auth\LoginFormRequest;
 use App\Models\User;
 use App\Services\JWTService\JWTService;
 use Exception;
-use Firebase\JWT\JWT;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
